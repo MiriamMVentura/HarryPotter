cf10fe5732d9c8ff572a3b6a1a29cd486b2cdaf4
MiriamMVentura <miriamartv@gmail.com>
2021-06-13 17:11:36 -0500

    feat(header): add logo and filter buttons

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../assets/Harry_Potter_wordmark 1.png';
+import Navbar from './Navbar';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <section className='header-top'>
+        <button className='header-top-btnFav'>FAVORITOS</button>
+        <button className='header-top-btnAdd'>AGREGAR</button>
+      </section>
+      <section className='header-logo'>
+        <section className='header-logo-img'>
+          <a href='/'>
+            <img src={logo} className='header-img'/>
+          </a>
+        </section>
+        <section className='header-navbar'>
+          <Navbar />
+        </section>
+      </section>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <p>Selecciona tu filtros</p>
+
+      <button>Estudiantes</button>
+      <button>STAFF</button>
+
+    </div>
+  )
+}
+
+export default Navbar;
